339a946574883dbfdd5cc16591688cef4fce3271
StephanieDeGraaf <sdegraaf@iastate.edu>
2016-09-14 12:29:01 -0700

    did significance tests for midratios

--- midratio.R
+++ midratio.R
@@ -1,4 +1,4 @@
-data<- read.csv("Bishayee Colony Counts 10.27.97-3.8.01.csv")
+data<- read.csv("preclinical-data-review\\Bishayee Colony Counts 10.27.97-3.8.01.csv")
 cols<- as.matrix(data[,c("col1", "col2", "col3")])
 
 midratio<- function(row){
@@ -8,10 +8,17 @@ midratio<- function(row){
   midratio<- (mid-low)/(high-low)
   return(midratio)
 }
-data$midratio<- sapply(1:dim(cols)[1], midratio)
+data$midratio<- as.numeric(as.character(sapply(1:dim(cols)[1], midratio)))
 # histogram of RTS midratio values: looks the same as in the paper
 hist(as.numeric(as.character(data$midratio)), breaks = 30, 
      xlab = "Mid-ratio values", main = "Histogram of RTS Mid-ratio Values")
+
+# conduct significance test based on binomial 
+# how many midratios are between [0.4, 0.6]
+n<- length(na.omit(data$midratio))
+p<- 0.26
+mr<- length(which(data$midratio <= 0.6 & data$midratio >= 0.4))
+prob<- 1-pbinom(mr, size=n, prob=p)
 
 
 others<- read.csv("Other Investigators in Lab.Colony Counts.4.23.92-11.27.02.csv")
@@ -28,3 +35,17 @@ outside$midratio<- sapply(1:dim(cols)[1], midratio)
 # histogram of Outside midratio values
 hist(as.numeric(as.character(outside$midratio)), breaks = 20,
      xlab = "Mid-ratio values", main = "Histogram of Others Mid-ratio Values")
+
+
+#look at Coulter counts as well:
+coult<- read.csv("Bishayee Coulter Counts.10.20.97-7.16.01.csv")
+cols<- as.matrix(coult[, c("Count.1", "Count.2", "Count.3")])
+coult$midratio<- as.numeric(as.character(sapply(1:dim(cols)[1], midratio)))
+# histogram of Outside midratio values
+hist(as.numeric(as.character(coult$midratio)), breaks = 20,
+     xlab = "Mid-ratio values", main = "Histogram of Coulter Count Mid-ratio Values")
+# binomial significance test for coulters:
+n<- length(na.omit(coult$midratio))
+p<- 0.26
+mr<- length(which(coult$midratio <= 0.6 & coult$midratio >= 0.4))
+prob<- 1-pbinom(mr, size=n, prob=p)
