cae1add3108de78a2e6c8403c1f676063559dd9e
James Hicks <james.hicks@berkeley.edu>
2016-09-22 08:38:39 -0700

    add range>1 limitation

--- poisson.R
+++ poisson.R
@@ -4,33 +4,18 @@
 require(poibin)
 require(tidyr)
 
-# First, estimate the MidProb bound for a range of lambda
-# (1) analytic - currently unfinished!
+# First, estimate the MidProb bound for a range of lambda using simulation
 
-is.odd <- function (x) x %% 2 != 0
-
-midprob <- function (lambda, j, N) {
-  prob <- NA
-  for (j in 2:N) {
-    prob[j] <- ppois(k - j, lambda) *
-    (ppois(k - (j / 2), lambda) + is.odd(j) * ppois(k - (j / 2) - 1, lambda)) *
-    ppois(k, lambda)
-  }
-  out <- 6 * sum(prob)
-  return(out)
-}
-
-# (2) simulation
-
-bound <- rep(NA, 1000)
-names(bound) <- seq(1:1000)
-for (lambda in 1:1000) {
-  probs <- matrix(rpois(3 * 5000, lambda), ncol = 3, byrow = TRUE)
-  logic <- apply(probs, 1, function (x) round(mean(x)) %in% x)
+bound <- rep(NA, 2000)
+names(bound) <- seq(1:2000)
+for (lambda in 1:2000) {
+  probs <- matrix(rpois(3 * 10000, lambda), ncol = 3, byrow = TRUE)
+  logic <- apply(probs, 1, function (x) round(mean(x)) %in% x & range(x)>1)
   bound[lambda] <- sum(logic) / length(logic)
 }
 
 # Input colony counts for rts and other
+
 colony <- read.csv("Bishayee Colony Counts 10.27.97-3.8.01.csv")
 colony <- colony %>% drop_na(col1, col2, col3)
 colony.other <- read.csv("Other Investigators in Lab.Colony Counts.4.23.92-11.27.02.csv")
@@ -47,10 +32,11 @@ rts.probs <- bound[rts.means]
 rts.k <- sum(apply(colony[, c("col1", "col2", "col3")],
                     1, function (x) round(mean(x)) %in% x))
 
-1 - pbinom(rts.k - 1, length(rts.means), max(rts.probs))
+rts.k
+length(rts.means)
+max(rts.probs)
 
-# doesn't replicate because the highest probability — i.e., our bound — is around 0.78 (for lambda = 1)
-# of course, as lambda grows, p quickly drops off, and most triples had considerably higher lambda
+1 - pbinom(rts.k - 1, length(rts.means), max(rts.probs))
 
 # Test II
 # Estimate probabilities using lambda MLE
@@ -59,14 +45,14 @@ rts.k <- sum(apply(colony[, c("col1", "col2", "col3")],
 # 3: Feed that vector into -ppoibin- to calculate probability for P-B distribution being more than value in RTS and other, respectively
 
 # RTS investigator
-1 - ppoibin(k - 1, rts.probs)
+1 - ppoibin(rts.k - 1, rts.probs)
 
 # Other investigators
 other.means <- round(colony.other$average)
 other.probs <- bound[other.means]
 other.k <- sum(apply(colony.other[, c("col1", "col2", "col3")],
                1, function (x) round(mean(x)) %in% x))
-1 - ppoibin(k - 1, other.probs)
+1 - ppoibin(other.k - 1, other.probs)
 
 # Test III
 # Normal approximation of poisson binomial
